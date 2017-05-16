9ac1df4c6c0118a6d2cb5f5387f25c1954c3d40d
Aetf <aetf@unlimitedcodeworks.xyz>
2017-05-16 15:48:52 -0400

    Add a fake cpu device under the name rpc

--- tensorflow/core/common_runtime/rpc_device/rpc_device_factory.cpp
+++ tensorflow/core/common_runtime/rpc_device/rpc_device_factory.cpp
@@ -1,5 +1,6 @@
 #include "tensorflow/core/common_runtime/device_factory.h"
 #include "tensorflow/core/common_runtime/rpc_device/rpc_device.h"
+#include "tensorflow/core/common_runtime/rpc_device/threadpiscine_device.h"
 
 namespace tensorflow {
 
@@ -15,14 +16,15 @@ class RpcDeviceFactory : public DeviceFactory {
     for (int i = 0; i < n; i++) {
       string name = strings::StrCat(name_prefix, "/device:RPC:", i);
       devices->push_back(
-          new RPcDevice(options, name, Bytes(256 << 20), DeviceLocality(),
-                        RpcLDevice::GetShortDeviceDescription(),
-                        cl::sycl::gpu_selector(), cpu_allocator()));
+//           new RpcDevice(options, name, Bytes(256 << 20), DeviceLocality(),
+//                         RpcLDevice::GetShortDeviceDescription(),
+//                         cl::sycl::gpu_selector(), cpu_allocator()));
+          new ThreadPiscineDevice(options, name, Bytes(256 << 20), DeviceLocality(), cpu_allocator()));
     }
     return Status::OK();
   }
 };
 
 REGISTER_LOCAL_DEVICE_FACTORY("RPC", RpcDeviceFactory, 200);
-}
 
+}  // namespace tensorflow
